b0643db91ce100716593b30851f7899ff53e1e15
ipratikner <pratiknerlikar@gmail.com>
2021-11-30 09:22:32 +0530

    Addedcontact details added to

---
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contacts> contacts;
+
+    public List<Contacts> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contacts> contacts) {
+        this.contacts = contacts;
+    }
+}
+
